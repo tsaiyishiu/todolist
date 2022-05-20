190ddd4aacff4338872305596998d893a1b43321
yishiu <zxc86655050zxc@gmail.com>
2022-05-20 17:31:29 +0800

    vuejs 銜接測試

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,102 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>
+      {{ title }}<span class="hl">{{ span }}</span>
+    </h2>
+
+    <div class="controls">
+      <label for="spacing">{{ Spacing }}</label>
+      <input
+        id="spacing"
+        type="range"
+        name="spacing"
+        min="10"
+        max="200"
+        value="10"
+        data-sizing="px"
+        v-on:change= 'handleUpdate'
+      />
+        <!-- v-on:mousemove= 'handleUpdate' -->
+
+      <label for="blur">{{ blur }}:</label>
+      <input
+        id="blur"
+        type="range"
+        name="blur"
+        min="0"
+        max="25"
+        value="10"
+        data-sizing="px"
+      />
+
+      <label for="base">{{ color }}</label>
+      <input id="base" type="color" name="base" value="#ffc600" />
+    </div>
+
+    <img :src="src" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      src: "https://source.unsplash.com/7bwQXzbF6KE/800x500",
+      title: "Update CSS Variables with",
+      span: "JSJSJS",
+      Spacing: "Spacing:Spacing:",
+      blur: "BlurBlur",
+      color: "Base ColorColor",
+    };
+  },
+  methods: {
+    handleUpdate(){
+      
+      let input = document.querySelector('.controls #spacing')
+      console.log(input)
+      const suffix = input.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${input.name}`, input.value + suffix);
+    }
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+:root {
+  --base: #ffc600;
+  --spacing: 10px;
+  --blur: 10px;
+}
+
+img {
+  padding: var(--spacing);
+  background: var(--base);
+  filter: blur(var(--blur));
+}
+
+.hl {
+  color: var(--base);
+}
+
+/*
+    misc styles, nothing to do with CSS variables
+  */
+
+body {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #193549;
+  color: white;
+  font-family: "helvetica neue", sans-serif;
+  font-weight: 100;
+  font-size: 50px;
+}
+
+.controls {
+  margin-bottom: 50px;
+}
+
+input {
+  width: 100px;
 }
 </style>
